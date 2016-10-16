921597fc4aa871c60d7ca217e43f79fa13835af8
= <mitalipalekar1@gmail.com>
2016-10-16 13:01:32 -0700

    working version of database

--- insta-captioner/src/routes/getMoodQuotes.js
+++ insta-captioner/src/routes/getMoodQuotes.js
@@ -1,23 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
-const file = process.env.CLOUD_DIR + '/' _ 'test.db';
+const file = process.env.CLOUD_DIR + '/' + 'database.db';
 
-
-function* getMoodQuotes(mood) {
+//function getMoodQuotes(mood) {
+	var mood = 'Friendship';
 	var quotes = [];
 
-	const db = new sqlite3.Database('../../../database.db');
-	db.all('SELECT ')
-}
-
-getMoodQuotes('happy');
-
-module.exports = getMoodQuotes;
-
-	// let quotes = {};
-
-	// const db = new sqlite3.Database
-	// db.all('SELECT content FROM Quotes q JOIN Categories c ON q.category = c.id AND c.name = ' + mood, function(err, row) {
-	// quotes.add(row.content);
-	// })
-
-	// db.close();
+	var db = new sqlite3.Database('../database.db');
+	//console.log("Database Creation worked");
+	db.all('SELECT q.content FROM Quotes q JOIN Categories c ON q.category = c.id AND c.name = \'' + mood + '\'', function(err, rows) {
+		console.log("database thing entered");
+		rows.forEach(function (row) {  
+	       quotes.push(row.content);  
+	    })
+	    console.log(quotes.toString());
+	});
+	db.close();
+//}
